85e38a38ee3d75fada9c2223d758f9ac2d523604
joaombc <65778250+joaombc@users.noreply.github.com>
2021-04-09 09:40:27 -0300

    Update regioesgraph.py

--- regioesgraph.py
+++ regioesgraph.py
@@ -3,13 +3,11 @@ import pandas as pd
 import plotly.offline as py
 import plotly.graph_objs as go
 import plotly.express as px
-import numpy as np
 
 py.init_notebook_mode(connected=True)
 
 #Utilizamos o pandas para abrir o arquivo csv
 df = pd.read_csv("https://raw.githubusercontent.com/joaombc/APC2021/main/regioes.csv")
-
 
 #Colocamos os dados dessa tabela em uma lista dentro de uma outra lista
 dados = df.values
@@ -28,26 +26,24 @@ jan_2020 = dados[:,1]#Quantidade de passageiros em Jan 2020
 jan_2021 = dados[:,2]#Quantidade de passageiros em Jan 2021
 variacao = dados[:,3]#Variação (%) da quantidade de passageiros
 
-#Gráfico um começa aqui
+
 barra_jan_2020 = go.Bar(x = regiao,
                 y = jan_2020,
                 name = 'Jan 2020',
                 marker = {'color': '#774ef0'})#barra jan 2020
+
 barra_jan_2021 = go.Bar(x = regiao,
                 y = jan_2021,
                 name = 'Jan 2021',
                 marker = {'color': '#ff4e39'})#barra jan 2021
 
-data = [barra_jan_2020, barra_jan_2021]
-#Gráfico um terminou
-
-#Gráfico dois começa aqui
 linha = go.Scatter(x = regiao,
                     y = variacao,
                     mode = 'lines',
                     name = '',
                     line = {'color': '#ee5253',
                             'dash': 'dash'})#linha
+
 pontos = go.Scatter(x = regiao,
                    y = variacao,
                    mode = 'markers',
@@ -57,24 +53,41 @@ pontos = go.Scatter(x = regiao,
                                         'color': '#000000'}},
                    opacity=.8)#ponto
 
-data2 = [linha, pontos]#gráfico dois terminou
+stack1 = go.Bar(x = regiao,
+                y = jan_2020,
+                name = 'Jan 2020',
+                marker = {'color': '#774ef0'})
 
-#"estilizar" o gráfico 1
+stack2 = go.Bar(x = regiao,
+                y = jan_2021,
+                name = 'Jan 2021',
+                marker = {'color': '#ff4e39'})
+
 layout = go.Layout(title = 'Movimento das Regiões',#Adiciona um título ao gráfico
                    xaxis = {'title': 'Regiões'},#adiciona um 'sentido' a x
                    yaxis = {'title': 'Movimentação de passageiros'},#adiciona um 'sentido' a y
                    xaxis_tickangle=-45)#'angulação do estados para melhor visualização
 
-#Imprimir o gráfico 1
-fig = go.Figure(data=data, layout=layout)
-py.iplot(fig)
-
-#"estilizar" o gráfico 2
 layout2 = go.Layout(title = 'Variação das Regiões',#Adiciona um título ao gráfico
                    xaxis = {'title': 'Regiões'},#adiciona um 'sentido' a x
-                   yaxis = {'title': 'Variação (%)'},#adiciona um 'sentido' a y
+                   yaxis = {'title': 'Variação da quantidade de passageiros (%)'},#adiciona um 'sentido' a y
                    xaxis_tickangle=-45)#'angulação do estados para melhor visualização
 
-#Imprimir o gráfico 2
+layout3 = go.Layout(title = 'Somatório Jan 2020 & Jan 2021',
+                   xaxis = {'title': 'Aeródromos'},
+                   yaxis = {'title': 'Movimentação de passageiros'},
+                   barmode = 'stack')
+
+data = [barra_jan_2020, barra_jan_2021]
+data2 = [linha, pontos]
+data3 = [stack1, stack2]
+
+fig = go.Figure(data=data, layout=layout)
 fig2 = go.Figure(data=data2, layout=layout2)
+fig3 = go.Figure(data=data3, layout=layout3)
+
+py.iplot(fig3)
+py.iplot(fig)
 py.iplot(fig2)
+
+
